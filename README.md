42356a83d7dbf46cf21e6e57c544d4c81c79bcaf
Mahdi <RuizMahdi@gmx.com>
2024-04-27 12:56:36 -0400

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,50 @@
+
+> Open this page at [https://mrdev88.github.io/bit-os/](https://mrdev88.github.io/bit-os/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/mrdev88/bit-os** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/mrdev88/bit-os** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Open this page at [https://mrdev88.github.io/bit-os/](https://mrdev88.github.io/bit-os/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/mrdev88/bit-os** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/mrdev88/bit-os** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,116 @@
+input.onGesture(Gesture.EightG, function () {
+    basic.showString("Steps: " + Step)
+})
+buttonClicks.onButtonDoubleClicked(buttonClicks.AorB.A, function () {
+    music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C5 - A - F - D - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C5 - A - F - D - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C D E F E D C - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C D E F E D C - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C5 B A G A B C5 - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C5 B A G A B C5 - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    datalogger.log(
+    datalogger.createCV("Steps", Step),
+    datalogger.createCV("Running Time (ms)", input.runningTime()),
+    datalogger.createCV("Tempature (C)", input.temperature()),
+    datalogger.createCV("Light Level", input.lightLevel()),
+    datalogger.createCV("Running Time (micros)", input.runningTimeMicros())
+    )
+    basic.showString("Saving Data")
+})
+input.onButtonPressed(Button.A, function () {
+    Heading = input.compassHeading()
+    basic.showString("Heading: " + Heading)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Record")
+    record.startRecording(record.BlockingState.Blocking)
+    record.playAudio(record.BlockingState.Blocking)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Images")
+    myImage = images.createImage(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    myImage.scrollImage(1, 200)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+    basic.showIcon(IconNames.House)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Scissors)
+    basic.pause(1000)
+    basic.showIcon(IconNames.LeftTriangle)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Diamond)
+    basic.pause(1000)
+    basic.showIcon(IconNames.SmallDiamond)
+})
+function Stats () {
+    basic.showString("Stats")
+    basic.showString("Running Time: " + input.runningTime() + " ms")
+    basic.showString("Running Time (micros): " + input.runningTimeMicros() + "micros")
+    basic.showString("Temperature (C)" + input.temperature())
+    basic.showString("Light Level: " + input.lightLevel())
+    basic.showString("Sound Level: " + input.soundLevel())
+}
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showString("Connect a Neopixel to pin 1")
+    pins.setEvents(DigitalPin.P1, PinEventType.Edge)
+    strip.showRainbow(1, 360)
+    for (let index = 0; index < 1000; index++) {
+        strip.show()
+        strip.rotate(1)
+        basic.pause(100)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    Step += 1
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Stats()
+})
+let myImage: Image = null
+let Heading = 0
+let Step = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P1, 8, NeoPixelMode.RGB)
+pins.setEvents(DigitalPin.P1, PinEventType.Touch)
+record.setMicGain(record.AudioLevels.High)
+datalogger.setColumnTitles(
+"Steps",
+"Running Time (ms)",
+"Tempature (C)",
+"Light Level",
+"Running Time (micros)"
+)
+basic.showString("Hello")
